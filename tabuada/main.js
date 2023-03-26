368b53b012352aa2e6f269ddd7016577af2c1b97
douglassqueiroz <douglas.queiroz18@outlook.com>
2023-03-25 21:13:46 -0400

    Adicionando a calculadora

---
+++ tabuada/main.js
@@ -0,0 +1,6 @@
+for(var i = 1; i < 11; i ++){
+    document.write("<p> Tabuada do " + i + "</p>");
+    for (var x = 1; x < 11; x++){
+        document.write(i + " x " + x + " = " + (i * x)+ "<br>");
+    }
+} 
